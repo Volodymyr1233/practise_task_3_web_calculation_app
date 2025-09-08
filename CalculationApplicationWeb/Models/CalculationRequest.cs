65d7eb168fa1493bc792c31eeb9c05348ca03984
Volodymyr1233 <105359387+Volodymyr1233@users.noreply.github.com>
2025-09-08 19:56:55 +0200

    Add project files.

---
+++ CalculationApplicationWeb/Models/CalculationRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace CalculationApplicationWeb.Models
+{
+    public class CalculationRequest
+    {
+        public class CalculationResponse
+        {
+            public int result { get; set; }
+        }
+       
+
+        public static async Task<int> SendRequest(int firstNumber, int secondNumber, string operation)
+        {
+            string base_url = "https://localhost:7007/api/Calculation/";
+            using (var client = new HttpClient())
+            {
+                var requestBody = new
+                {
+                    a = firstNumber,    
+                    b = secondNumber
+                };
+                var json = JsonSerializer.Serialize(requestBody);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                string operationString = operation.ToLower() switch
+                {
+                    "+" => "add",
+                    "-" => "subtract",
+                    "*" => "multiply",
+                    _ => throw new ArgumentException("Invalid operation")
+                };
+                var response = await client.PostAsync(base_url + operationString, content);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var calculationResponse = JsonSerializer.Deserialize<CalculationResponse>(responseBody);
+                return calculationResponse.result;
+            }
+        }
+    }
+}
---
+++ CalculationApplicationWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculationApplicationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
